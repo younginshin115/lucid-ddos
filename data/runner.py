0cf8350f9a8fc1f539b97d970790c0936f2b5dd6
younginshin115 <younginshin115@gmail.com>
2025-04-09 21:54:34 +0900

    refactor: extract preprocess_dataset_from_data() into runner.py

--- data/runner.py
+++ data/runner.py
@@ -1,10 +1,29 @@
+# Copyright (c) 2022 @ FBK - Fondazione Bruno Kessler
+# Author: Roberto Doriguzzi-Corin
+# Project: LUCID: A Practical, Lightweight Deep Learning Solution for DDoS Attack Detection
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import numpy as np
+import os, glob, time, pickle, h5py
+from multiprocessing import Process, Manager
+
 from data.parser import parse_labels
 from data.process_pcap import process_pcap
-from data.flow_utils import count_flows
-import numpy as np
-import os, glob, time, pickle
-from multiprocessing import Process, Manager
-from utils.constants import MAX_FLOW_LEN, TIME_WINDOW
+from data.flow_utils import count_flows, balance_dataset, dataset_to_list_of_fragments
+from data.split import train_test_split
+from utils.preprocessing import normalize_and_padding
+from utils.minmax_utils import static_min_max
+from utils.constants import MAX_FLOW_LEN, TIME_WINDOW, TRAIN_SIZE
 
 def parse_dataset_from_pcap(args, command_options):
     manager = Manager()
@@ -68,3 +87,79 @@ def parse_dataset_from_pcap(args, command_options):
     print(log_string)
     with open(os.path.join(output_folder, 'history.log'), 'a') as myfile:
         myfile.write(log_string)
+
+
+def preprocess_dataset_from_data(args, command_options):
+
+    # 파일 목록과 output 디렉토리 결정
+    if args.preprocess_folder:
+        filelist = glob.glob(args.preprocess_folder[0] + '/*.data')
+        output_folder = args.output_folder[0] if args.output_folder else args.preprocess_folder[0]
+    else:
+        filelist = args.preprocess_file
+        output_folder = args.output_folder[0] if args.output_folder else os.path.dirname(os.path.realpath(filelist[0]))
+
+    # 파일명에서 time_window, flow_len, dataset_id 추출
+    time_window, max_flow_len, dataset_id = None, None, None
+    for file in filelist:
+        parts = os.path.basename(file).split('-')
+        current_time_window = int(parts[0].replace('t', ''))
+        current_flow_len = int(parts[1].replace('n', ''))
+        current_dataset_id = parts[2]
+
+        if time_window and time_window != current_time_window:
+            print("Inconsistent time windows!!"); exit()
+        if max_flow_len and max_flow_len != current_flow_len:
+            print("Inconsistent flow lengths!!"); exit()
+
+        time_window = current_time_window
+        max_flow_len = current_flow_len
+        dataset_id = dataset_id if dataset_id and dataset_id == current_dataset_id else "IDS201X"
+
+    # .data 로드
+    preprocessed_flows = []
+    for file in filelist:
+        with open(file, 'rb') as fh:
+            preprocessed_flows += pickle.load(fh)
+
+    # 밸런싱 + 샘플 수 제한
+    preprocessed_flows, _, _ = balance_dataset(preprocessed_flows, args.samples)
+
+    if not preprocessed_flows:
+        print("Empty dataset after balancing.")
+        exit()
+
+    # split
+    train, test = train_test_split(preprocessed_flows, train_size=TRAIN_SIZE)
+    train, val = train_test_split(train, train_size=TRAIN_SIZE)
+
+    # flatten
+    X_train, y_train, _ = dataset_to_list_of_fragments(train)
+    X_val, y_val, _ = dataset_to_list_of_fragments(val)
+    X_test, y_test, _ = dataset_to_list_of_fragments(test)
+
+    # normalize + padding
+    mins, maxs = static_min_max(time_window)
+    norm_X_train = np.array(normalize_and_padding(X_train, mins, maxs, max_flow_len))
+    norm_X_val = np.array(normalize_and_padding(X_val, mins, maxs, max_flow_len))
+    norm_X_test = np.array(normalize_and_padding(X_test, mins, maxs, max_flow_len))
+
+    y_train = np.array(y_train)
+    y_val = np.array(y_val)
+    y_test = np.array(y_test)
+
+    # 저장
+    filename_prefix = f"{time_window}t-{max_flow_len}n-{dataset_id}-dataset"
+    for split, X, y in zip(["train", "val", "test"], [norm_X_train, norm_X_val, norm_X_test], [y_train, y_val, y_test]):
+        with h5py.File(os.path.join(output_folder, f"{filename_prefix}-{split}.hdf5"), 'w') as hf:
+            hf.create_dataset('set_x', data=X)
+            hf.create_dataset('set_y', data=y)
+
+    # 로그
+    total = len(y_train) + len(y_val) + len(y_test)
+    ddos = np.count_nonzero(np.concatenate([y_train, y_val, y_test]))
+    print_str = f"{time.strftime('%Y-%m-%d %H:%M:%S')} | Total examples: {total} (ddos: {ddos}) | options: {command_options}"
+    print(print_str)
+
+    with open(os.path.join(output_folder, 'history.log'), 'a') as logf:
+        logf.write(print_str + '\n')
--- lucid_dataset_parser.py
+++ lucid_dataset_parser.py
@@ -14,23 +14,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import sys
 import time
-import pickle
 import random
 import glob
 import h5py
 import numpy as np
 
-from utils.constants import MAX_FLOW_LEN, TIME_WINDOW, TRAIN_SIZE, SEED
-from utils.preprocessing import normalize_and_padding
-from utils.minmax_utils import static_min_max
+from utils.constants import MAX_FLOW_LEN, TIME_WINDOW, SEED
 from data.data_loader import count_packets_in_dataset
-from data.flow_utils import dataset_to_list_of_fragments, balance_dataset
-from data.split import train_test_split
 from data.args import get_dataset_parser, validate_args
-from data.runner import parse_dataset_from_pcap
+from data.runner import parse_dataset_from_pcap, preprocess_dataset_from_data
 
 
 # Sample commands
@@ -48,144 +42,11 @@ def main(argv):
     args = parser.parse_args()
     validate_args(args, parser)
 
-
-    if args.packets_per_flow is not None:
-        max_flow_len = int(args.packets_per_flow[0])
-    else:
-        max_flow_len = MAX_FLOW_LEN
-
-    if args.time_window is not None:
-        time_window = float(args.time_window[0])
-    else:
-        time_window = TIME_WINDOW
-
-    if args.dataset_id is not None:
-        dataset_id = str(args.dataset_id[0])
-    else:
-        dataset_id = ''
-
     if args.dataset_folder and args.dataset_type:
         parse_dataset_from_pcap(args, command_options)
 
-    if args.preprocess_folder is not None or args.preprocess_file is not None:
-        if args.preprocess_folder is not None:
-            output_folder = args.output_folder[0] if args.output_folder is not None else args.preprocess_folder[0]
-            filelist = glob.glob(args.preprocess_folder[0] + '/*.data')
-        else:
-            output_folder = args.output_folder[0] if args.output_folder is not None else os.path.dirname(os.path.realpath(args.preprocess_file[0]))
-            filelist = args.preprocess_file
-
-        # obtain time_window and flow_len from filename and ensure that all files have the same values
-        time_window = None
-        max_flow_len = None
-        dataset_id = None
-        for file in filelist:
-            filename = file.split('/')[-1].strip()
-            current_time_window = int(filename.split('-')[0].strip().replace('t',''))
-            current_max_flow_len = int(filename.split('-')[1].strip().replace('n',''))
-            current_dataset_id = str(filename.split('-')[2].strip())
-            if time_window != None and current_time_window != time_window:
-                print ("Incosistent time windows!!")
-                exit()
-            else:
-                time_window = current_time_window
-            if max_flow_len != None and current_max_flow_len != max_flow_len:
-                print ("Incosistent flow lengths!!")
-                exit()
-            else:
-                max_flow_len = current_max_flow_len
-
-            if dataset_id != None and current_dataset_id != dataset_id:
-                dataset_id = "IDS201X"
-            else:
-                dataset_id = current_dataset_id
-
-
-
-        preprocessed_flows = []
-        for file in filelist:
-            with open(file, 'rb') as filehandle:
-                # read the data as binary data stream
-                preprocessed_flows = preprocessed_flows + pickle.load(filehandle)
-
-
-        # balance samples and redux the number of samples when requested
-        preprocessed_flows, benign_fragments, ddos_fragments = balance_dataset(preprocessed_flows,args.samples)
-
-        if len(preprocessed_flows) == 0:
-            print("Empty dataset!")
-            exit()
-
-        preprocessed_train, preprocessed_test = train_test_split(preprocessed_flows,train_size=TRAIN_SIZE, shuffle=True)
-        preprocessed_train, preprocessed_val = train_test_split(preprocessed_train, train_size=TRAIN_SIZE, shuffle=True)
-
-        X_train, y_train, _ = dataset_to_list_of_fragments(preprocessed_train)
-        X_val, y_val, _ = dataset_to_list_of_fragments(preprocessed_val)
-        X_test, y_test, _ = dataset_to_list_of_fragments(preprocessed_test)
-
-        # normalization and padding
-        X_full = X_train + X_val + X_test
-        y_full = y_train + y_val + y_test
-        mins,maxs = static_min_max(time_window=time_window)
-
-        total_examples = len(y_full)
-        total_ddos_examples = np.count_nonzero(y_full)
-        total_benign_examples = total_examples - total_ddos_examples
-
-        output_file = output_folder + '/' + str(time_window) + 't-' + str(max_flow_len) + 'n-' + dataset_id + '-dataset'
-        if args.no_split == True: # don't split the dataset
-            norm_X_full = normalize_and_padding(X_full, mins, maxs, max_flow_len)
-            #norm_X_full = padding(X_full,max_flow_len) # only padding
-            norm_X_full_np = np.array(norm_X_full)
-            y_full_np = np.array(y_full)
-
-            hf = h5py.File(output_file + '-full.hdf5', 'w')
-            hf.create_dataset('set_x', data=norm_X_full_np)
-            hf.create_dataset('set_y', data=y_full_np)
-            hf.close()
-
-            [full_packets] = count_packets_in_dataset([norm_X_full_np])
-            log_string = time.strftime("%Y-%m-%d %H:%M:%S") + " | Total examples (tot,ben,ddos):(" + str(total_examples) + "," + str(total_benign_examples) + "," + str(total_ddos_examples) + \
-                         ") | Total packets:(" + str(full_packets) + \
-                         ") | options:" + command_options + " |\n"
-        else:
-            norm_X_train = normalize_and_padding(X_train,mins,maxs,max_flow_len)
-            norm_X_val = normalize_and_padding(X_val, mins, maxs, max_flow_len)
-            norm_X_test = normalize_and_padding(X_test, mins, maxs, max_flow_len)
-
-            norm_X_train_np = np.array(norm_X_train)
-            y_train_np = np.array(y_train)
-            norm_X_val_np = np.array(norm_X_val)
-            y_val_np = np.array(y_val)
-            norm_X_test_np = np.array(norm_X_test)
-            y_test_np = np.array(y_test)
-
-            hf = h5py.File(output_file + '-train.hdf5', 'w')
-            hf.create_dataset('set_x', data=norm_X_train_np)
-            hf.create_dataset('set_y', data=y_train_np)
-            hf.close()
-
-            hf = h5py.File(output_file + '-val.hdf5', 'w')
-            hf.create_dataset('set_x', data=norm_X_val_np)
-            hf.create_dataset('set_y', data=y_val_np)
-            hf.close()
-
-            hf = h5py.File(output_file + '-test.hdf5', 'w')
-            hf.create_dataset('set_x', data=norm_X_test_np)
-            hf.create_dataset('set_y', data=y_test_np)
-            hf.close()
-
-            [train_packets, val_packets, test_packets] = count_packets_in_dataset([norm_X_train_np, norm_X_val_np, norm_X_test_np])
-            log_string = time.strftime("%Y-%m-%d %H:%M:%S") + " | examples (tot,ben,ddos):(" + str(total_examples) + "," + str(total_benign_examples) + "," + str(total_ddos_examples) + \
-                         ") | Train/Val/Test sizes: (" + str(norm_X_train_np.shape[0]) + "," + str(norm_X_val_np.shape[0]) + "," + str(norm_X_test_np.shape[0]) + \
-                         ") | Packets (train,val,test):(" + str(train_packets) + "," + str(val_packets) + "," + str(test_packets) + \
-                         ") | options:" + command_options + " |\n"
-
-        print(log_string)
-
-        # saving log file
-        with open(output_folder + '/history.log', "a") as myfile:
-            myfile.write(log_string)
+    elif args.preprocess_folder or args.preprocess_file:
+        preprocess_dataset_from_data(args, command_options)
 
     if args.balance_folder is not None and args.output_folder is not None:
         output_folder = args.output_folder[0] if args.output_folder is not None else args.balance_folder[0]
