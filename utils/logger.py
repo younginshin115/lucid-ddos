5fd09c7faf54b45affb9016dec2f45d24feacc40
younginshin115 <younginshin115@gmail.com>
2025-04-09 18:23:35 +0900

    chore: retain original license and copyright headers in extracted modules

--- utils/logger.py
+++ utils/logger.py
@@ -1,3 +1,19 @@
+# Copyright (c) 2022 @ FBK - Fondazione Bruno Kessler
+# Author: Roberto Doriguzzi-Corin
+# Project: LUCID: A Practical, Lightweight Deep Learning Solution for DDoS Attack Detection
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import pprint
 import numpy as np
 from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
