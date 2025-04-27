cb4cde7ebee78ccd87e693f8c50702cf70ac5612
younginshin115 <younginshin115@gmail.com>
2025-04-27 20:26:21 +0900

    refactor(constants): remove unused hyperparameter grid to streamline code and improve clarity

--- utils/constants.py
+++ utils/constants.py
@@ -25,13 +25,6 @@ PATIENCE = 10
 DEFAULT_EPOCHS = 1000
 VAL_HEADER = ['Model', 'Samples', 'Accuracy', 'F1Score', 'Hyper-parameters','Validation Set']
 PREDICT_HEADER = ['Model', 'Time', 'Packets', 'Samples', 'DDOS%', 'Accuracy', 'F1Score', 'TPR', 'FPR','TNR', 'FNR', 'Source']
-HYPERPARAM_GRID = {
-    "optimizer__learning_rate": [0.1, 0.01],  # ← 여기에 넘겨야 실제로 optimizer에 반영됨
-    "batch_size": [1024,2048],
-    "model__kernels": [32,64],
-    "model__regularization" : [None,'l1'],
-    "model__dropout" : [None,0.2]
-}
 PROTOCOLS = [
     'arp', 'data', 'dns', 'ftp', 'http', 'icmp',
     'ip', 'ssdp', 'ssl', 'telnet', 'tcp', 'udp'
